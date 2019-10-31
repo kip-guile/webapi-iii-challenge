8db782ca6955aaa0a666150d040ac43ddaa9d26d
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-10-31 16:51:48 +0100

    ready for deployment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
+require('dotenv').config()
 const server = require("./server/server")
+const port = process.env.PORT
 
-server.listen(process.env.port || 3000, () => {
-    console.log('Listening to ' + (process.env.port || 3000))
+server.listen(port, () => {
+    console.log('Listening to ' + (port))
 })
